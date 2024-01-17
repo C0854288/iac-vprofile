24262c6537b2b7e59ba9bf43e4b2b58bde30dcc1
vishnuvardhansunkara <vishnusunkara17@gmail.com>
2024-01-17 14:08:23 -0500

    message

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -7,7 +7,7 @@ output "cluster_endpoint" {
   description = "Endpoint for Amazon Web Service EKS "
   value       = module.eks.cluster_endpoint
 }
-
+#####
 output "region" {
   description = "Amazon Web Service EKS Cluster region"
   value       = var.region
